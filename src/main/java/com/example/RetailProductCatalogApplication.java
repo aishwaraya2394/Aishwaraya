bb8e0a866849495701b4abc587d648a5ea98cce7
Aishwaraya <aishwaraya2394@gmail.com>
2024-11-17 19:19:24 +0100

    Changes for RetailProductCatalog setup: adding relevant comments

---
+++ src/main/java/com/example/RetailProductCatalogApplication.java
@@ -0,0 +1,15 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetailProductCatalogApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(RetailProductCatalogApplication.class, args);
+
+    }
+
+}
---
+++ src/main/resources/static/js/searchProduct.js
@@ -0,0 +1,99 @@
+let currentPage = 0;
+const pageSize =2;
+
+async function getAllProducts(){
+
+    try{
+        const response = await fetch(`/products`);
+        if(!response.ok){
+
+            throw new Error(`Error: $(response.status)`);
+        }
+        products = await response.json();
+
+        displayProducts(products);
+
+        document.byElementId('prevPage').addEventListener('click', function() => {
+
+            if(currentPage > 0){
+                currentPage--;
+                getAllProducts();
+            }
+
+        });
+
+        document.byElementId('nextPage').addEventListener('click', function() => {
+
+            currentPage++;
+            getAllProducts();
+
+        });
+
+    }
+    catch(error){
+
+        console.error('Error fetching items',error);
+    }
+
+}
+
+function displayProducts(products){
+
+    const tableBody = document.byElementId('productsTable');
+    tableBody.innerHTML = '';
+
+    products.forEach)(product => {
+        const row = document.createElement('tr');
+        row.innerHTML = `
+            <td>${product.id}</td>
+            <td><a href = "/products.html?id=${product.id}">${product.name}</td>
+            <td>${product.category}</td>
+            <td>${product.description}</td>
+            <td>${product.price}</td>
+            <td>${product.imageUrl}</td>
+            `;
+        tableBody.appendChild(row);
+    });
+}
+
+
+
+
+async function searchProduct(name = ' '){
+    try{
+        const responseByName = await fetch(`/search?name=${name}`);
+        if(!response.ok){
+
+            throw new Error(`Error: $(response.status)`);
+        }
+        productsByName = await responseByName.json();
+
+        displayProducts(productsByName);
+
+        document.byElementId('prevPage').addEventListener('click', function() => {
+
+            if(currentPage > 0){
+                currentPage--;
+                searchProduct();
+            }
+
+        });
+
+        document.byElementId('nextPage').addEventListener('click', function() => {
+
+            currentPage++;
+            searchProduct();
+
+        });
+    }
+    catch(error){
+        console.error('Error fetching product:',error);
+    }
+}
+
+document.byElementId('searchBar').addEventListener('input', function () => {
+    const name = this.value;
+    searchProduct(name);
+});
+
+
